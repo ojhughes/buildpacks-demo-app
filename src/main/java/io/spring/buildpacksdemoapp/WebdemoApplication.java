87ce219dc3d9c3214c45aa77ebaca6342a4589c4
Ollie Hughes <olli.hughes@gmail.com>
2019-04-16 12:39:19 +0100

    Sample app for testing cloud native build packs

---
+++ src/main/java/io/spring/buildpacksdemoapp/WebdemoApplication.java
@@ -0,0 +1,14 @@
+package io.spring.buildpacksdemoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebdemoApplication.class, args);
+	}
+
+}
+
